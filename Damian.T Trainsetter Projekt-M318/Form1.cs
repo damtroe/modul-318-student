38486b97ec04c05bd89a88256e02fd8f4f80fea9
damtroe <datroe95@web.de>
2017-05-03 08:43:47 +0200

    Functions Added Signed-off-by: damtroe <datroe95@web.de>

--- Damian.T Trainsetter Projekt-M318/Form1.cs
+++ Damian.T Trainsetter Projekt-M318/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwissTransport;
 
 namespace Damian.T_Trainsetter_Projekt_M318
 {
@@ -15,6 +16,61 @@ namespace Damian.T_Trainsetter_Projekt_M318
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Text = Convert.ToString(DateTime.Now.Hour);
+    }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string suche = textBox1.Text;
+
+            ITransport testee = new Transport();
+            var stations = testee.GetStations(suche);
+            foreach (Station s in stations.StationList)
+            {
+                listBox1.Items.Add(": " + s.Name + "X:" + s.Coordinate.XCoordinate.ToString() + "Score" + s.Score);
+
+
+            }     
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string suche = textBox1.Text;
+
+            ITransport testee = new Transport();
+            var stations = testee.GetStations(suche);
+            foreach (Station s in stations.StationList)
+            {
+                listBox1.Items.Add(" " + s.Name + "X:" + s.Coordinate.XCoordinate.ToString() + "Score" + s.Score);
+
+
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+    }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = Convert.ToString(DateTime.Now.Hour) + " : " + Convert.ToString(DateTime.Now.Minute) + " : " + Convert.ToString(DateTime.Now.Second);
         }
     }
 }
